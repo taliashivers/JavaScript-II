347150eb3042ae5923d447445e5e5844c780eb13
[taliashivers] <[talia.trembley@gmail.com]>
2019-01-21 17:41:42 -0700

    Finished Callbacks

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -33,22 +33,42 @@ getLength(items, (lengthOfList) => {
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
-items.forEach((item, index) => {
-  alert(item, index);
+  cb(arr[arr.length-1]);
+}
+
+last(items, (lastItem) => {
+  console.log(lastItem);
 });
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
-}
+    cb(x + y);
+    }
+  
+  sumNums(2, 6, sumOfXY => {
+    console.log(sumOfXY);
+  });
+  
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  cb(x*y);
 }
+
+multiplyNums(2, 6, productXY => {
+  console.log(productXY);
+});
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
-}
+  cb(list.includes(item));
+};
+
+contains("Gum", items, includes => {
+  console.log(includes);
+});
+
 
 /* STRETCH PROBLEM */
 
